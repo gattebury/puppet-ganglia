3ffe8db529b79f50587a8ad91f97893272d30e57
Trey Dockendorf <tdockendorf@osc.edu>
2017-05-17 13:29:37 -0400

    Fix usage of validate_string to work with newer stdlib, avoid validate_string of undef

--- manifests/gmetad.pp
+++ manifests/gmetad.pp
@@ -26,7 +26,9 @@ class ganglia::gmetad(
   validate_string($gmetad_service_config)
   validate_string($gmetad_user)
   validate_integer($gmetad_case_sensitive_hostnames, 1, 0)
-  validate_string($gmetad_status_command)
+  if $gmetad_status_command {
+    validate_string($gmetad_status_command)
+  }
 
   if ($::ganglia::params::gmetad_status_command) {
     $hasstatus = false
